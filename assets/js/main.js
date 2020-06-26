8a3c4185673a5ac01a2d5f593bd8c58b6582e8dd
Rannojit <cserannojitsarkar22@gmail.com>
2020-06-27 03:49:21 +0600

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,20 +19,44 @@
     $('.mHc5').matchHeight();
   };
 
-  $('.get-value').keyup(function(){
-    var takeValue = $(this).val(); 
-    $('.value-set').html('<h5>'+takeValue+'</h5');
-  });
-  
-  $(".why-sec-grd-cntlr ul li").slice(0,4).show();
-  $("#seeMore").click(function(e){
-    e.preventDefault();
-    $(".why-sec-grd-cntlr ul li:hidden").slice(0,4).fadeIn("slow");
+  if($('.get-value').length){
+    $('.get-value').keyup(function(){
+      var takeValue = $(this).val(); 
+      $('.value-set').html('<h5>'+takeValue+'</h5');
+    });
+  };
+  if($('.why-sec-grd-cntlr').length){
+    $(".why-sec-grd-cntlr ul li").slice(0,4).show();
+    $("#seeMore").click(function(e){
+      e.preventDefault();
+      $(".why-sec-grd-cntlr ul li:hidden").slice(0,4).fadeIn("slow");
+      
+      if($(".why-sec-grd-cntlr ul li:hidden").length == 0){
+         $("#seeMore").fadeOut("slow");
+        }
+    });
+  };
     
-    if($(".why-sec-grd-cntlr ul li:hidden").length == 0){
-       $("#seeMore").fadeOut("slow");
-      }
-  });
 
+
+    /*var t = $('.acceess-form-fields input').prop('required',true);
+    alert(t);
+    if( t == true){
+      $('.acceess-submit-btn button').on('click', function(){
+        $('.page-acceess-con-cntlr').hide();
+      });
+    }*//*else{
+      $(".alert-txt").append( "<p>Ημερομηνια γεννησης</p>" );
+    }*/
+    /*$('.access-year').keyup(function(){
+      //var inputFillUp = $('.acceess-form-fields input[name="acceess"]')[0].checked = true;
+      
+      var yearTakeValue = $(this).val();
+      if (yearTakeValue <= 2004){
+        $('.acceess-submit-btn button').on('click', function(){
+          $('.page-acceess-con-cntlr').hide();
+        });
+      }
+    });*/
 
 })(jQuery);
